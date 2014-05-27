2e8d591e2e643db7e36c0113d3a3899d499392f8
eanderton <eric.t.anderton@gmail.com>
2014-05-26 21:28:55 -0400

    Code cleanup

--- src/grapnel/util.go
+++ src/grapnel/util.go
@@ -8,50 +8,8 @@ import (
   "os/exec"
   "path/filepath"
   "regexp"
-  "sync"
   log "github.com/ngmoco/timber"
 )
-
-type Condition struct {
-  value bool
-  cond *sync.Cond
-  mutex *sync.Mutex
-}
-
-func NewCondition() *Condition {
-  self := &Condition{}
-  self.value = false
-  self.mutex = &sync.Mutex{}
-  self.cond = sync.NewCond(self.mutex)
-  return self
-}
-
-func (self *Condition) Wait() {
-  self.mutex.Lock()
-  for !self.value {
-    self.cond.Wait()
-  }
-  self.mutex.Unlock()
-}
-
-func (self *Condition) Get() bool {
-  self.mutex.Lock()
-  defer self.mutex.Unlock()
-  return self.value
-}
-
-func (self *Condition) Fire() {
-  self.mutex.Lock()
-  self.value = true
-  self.mutex.Unlock()
-  self.cond.Broadcast()
-}
-
-func (self *Condition) Reset() {
-  self.mutex.Lock()
-  self.value = true
-  self.mutex.Unlock()
-}
 
 type RunContext struct {
   WorkingDirectory string
@@ -78,72 +36,6 @@ func (self *RunContext) Run(cmd string, args... string) error {
   }
   return err
 }
-
-/*
-// Function chain for deferred callback registration
-// Designed to be used with 'defer', to allow closures and
-// called functions to register defer/cleanup functions
-// within a parent scope
-type FnChain struct {
-  chain []func()
-}
-
-// FnChain constructor
-func NewFnChain() *FnChain {
-  return &FnChain{}
-}
-
-// Adds a func to the chain
-// TODO: change to LIFO order
-func (self *FnChain) Add(fn func()) {
-  self.chain = append(self.chain, fn)
-}
-
-// Calls all functions on the chain
-// TODO: handle panic/recover to allow all chain elements to be called
-func (self *FnChain) Invoke() {
-  for _, fn := range self.chain {
-    fn()
-  }
-}
-
-func (self *FnChain) Clear() {
-  self.chain = self.chain[0:0]
-}
-
-//  Creates a temporary directory
-// Destroys the temporary directory when the chain is invoked
-func createTempDir(deferChain *FnChain) (string, error) {
-  tempRoot, err := ioutil.TempDir("")
-  tempRoot = filepath.Join(tempRoot, suffix)
-  if err != nil {
-    log.Info("%s", err.Error())
-    return "", log.Error("Could not create temporary directory: '%s'", tempRoot)
-  }
-  deferChain.Add(func() {
-    log.Info("Cleaning up '%s'", tempRoot)
-    os.RemoveAll(tempRoot)
-  })
-  return tempRoot, err
-}
-
-// TODO: tighten up this implementation
-func pushDir(newDir string, deferChain *FnChain) error {
-  // save the current working directory
-  oldDir, err := os.Getwd()
-  if err != nil {
-    return err
-  }
-  // change to new directory
-  if err := os.Chdir(newDir); err != nil {
-    return err
-  }
-  deferChain.Add(func() {
-    os.Chdir(oldDir)
-  })
-  return nil
-}
-*/
 
 // Copies a file tree from src to dest
 func CopyFileTree(dest string, src string, ignore string) error {
